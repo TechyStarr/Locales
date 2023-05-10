5f006ceb251e3999ed6fb9286ab516c0ed98671d
Calista Ifenkwe <calistaifenkwe@gmail.com>
2023-05-10 06:29:19 +0100

    changes

--- api/__init__.py
+++ api/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_restx import Api
-from .config.config import config_dict
+# from .config.config import config_dict
 from .utils import db
 
 
 
-def create_app(config=config_dict['dev']):
+def create_app():
     app = Flask(__name__)
 
-    app.config.from_object(config)
+    # app.config.from_object(config)
 
     db.init_app(app)
 
--- api/config/config.py
+++ api/config/config.py
@@ -1,39 +1,39 @@
-import os
-from decouple import config
-from datetime import timedelta
+# import os
+# # from decouple import config
+# from datetime import timedelta
 
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+# BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-db_name = 'student_management_db'
-
-
-
-class Config:
-    SECRET_KEY = config('SECRET_KEY', 'secret')
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=90)
-    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
-    JWT_SECRET_KEY = config('JWT_SECRET_KEY')
-
-class DevConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-
-class TestConfig(Config):
-    TESTING = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite://' # An SQL memory database is used
-
-class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = uri
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = config('DEBUG', False, cast=bool)
+# db_name = 'student_management_db'
 
 
-config_dict = {
-    'dev': DevConfig,
-    'prod': ProdConfig,
-    'test': TestConfig
-}
+
+# class Config:
+#     SECRET_KEY = config('SECRET_KEY', 'secret')
+#     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=90)
+#     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
+#     JWT_SECRET_KEY = config('JWT_SECRET_KEY')
+
+# class DevConfig(Config):
+#     DEBUG = True
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SQLALCHEMY_ECHO = True
+#     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+# class TestConfig(Config):
+#     TESTING = True
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SQLALCHEMY_ECHO = True
+#     SQLALCHEMY_DATABASE_URI = 'sqlite://' # An SQL memory database is used
+
+# class ProdConfig(Config):
+#     # SQLALCHEMY_DATABASE_URI = uri
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     DEBUG = config('DEBUG', False, cast=bool)
+
+
+# config_dict = {
+#     'dev': DevConfig,
+#     'prod': ProdConfig,
+#     'test': TestConfig
+# }

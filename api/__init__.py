5ae3218083a6fed8a971e4e377e5c613580391ba
Calista Ifenkwe <calistaifenkwe@gmail.com>
2023-05-11 09:16:33 +0100

    fixed db issue

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_restx import Api
-from .config.config import config_dict
-# from .utils import db
 from .auth.views import auth_namespace
+from .config.config import config_dict
+from .utils.utils import db
 
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
@@ -15,24 +15,30 @@ def create_app(config=config_dict['dev']):
 
     app.config.from_object(config)
 
-    # db.init_app(app)
+    db.init_app(app)
 
+    jwt = JWTManager(app)
+
+    migrate = Migrate(app, db)
+
+    # authorizations = {
+    #     "Bearer Auth": {
+    #     "type": "apikey",
+    #     "in": "header",
+    #     "name": "Authorization", 
+    #     "description": "Add a JWT token to the header with ** Bearer &lt;JWT&gt; ** token to authorize user "
+    #     }
+    # }
 
     api = Api(app,
         title="Locale API", 
-        description="Find whatever you're looking for using Locale API",
+        description="Find whatever you're looking for using Locale",
         # authorization = authorizations,
         # security = "Bearer Auth"
     )
 
     api.add_namespace(auth_namespace, path='/auth')
 
-
-
-    @app.shell_context_processor 
-    def make_shell_context():
-        return {
-        }
 
 
     return app
--- api/auth/views.py
+++ api/auth/views.py
@@ -1,6 +1,18 @@
 from flask_restx import Resource, Namespace
 from flask import request
+from http import HTTPStatus
 
 
 auth_namespace = Namespace('auth', description='Authentication related operations')
+
+
+class test(Resource):
+
+    def post():
+        response = {
+            "message": "Hello World!"
+        }
+
+        return response, HTTPStatus.ACCEPTED
+
 
